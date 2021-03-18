a12abe8cb10786b70432a5819afdd334f5c5f19a
grigagod <podkidysh2002@gmail.com>
2021-03-18 09:43:08 +0300

    [*] another lib for DH

---
+++ client/client.py
@@ -0,0 +1,24 @@
+import json
+from coincurve import PrivateKey
+import binascii
+
+
+class Client:
+    def __init__(self, username):
+        self.username = username
+        self.init_keys()
+
+    def init_keys(self):
+        f = open(self.username + '.json')
+
+        data = json.load(f)
+
+        priv_key_hex = data["private_key"]
+
+        self.priv_key = PrivateKey.from_hex(priv_key_hex)
+
+        f.close()
+
+    def show_keys(self):
+        print(self.priv_key.to_hex())
+        print(binascii.hexlify(self.priv_key.public_key.format()))
--- client/main.py
+++ client/main.py
@@ -1,15 +1,4 @@
-from diffiehellman.diffiehellman import DiffieHellman
+import client
 
-alice = DiffieHellman()
-bob = DiffieHellman()
-
-alice.generate_public_key()    # automatically generates private key
-bob.generate_public_key()
-
-alice.generate_shared_secret(bob.public_key, echo_return_key=True)
-bob.generate_shared_secret(alice.public_key, echo_return_key=True)
-
-a = alice.shared_key
-print(a)
-b = bob.shared_key
-print(b)
+c = client.Client("user1")
+c.show_keys()
