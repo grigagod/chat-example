a9dea323e6a51ffbc842546e18957f07d8130047
grigagod <podkidysh2002@gmail.com>
2021-03-23 10:06:11 +0300

    [+] client model && initial requests

--- client/main.py
+++ client/main.py
@@ -1,4 +1,21 @@
-import client
+#!/usr/bin/env python
 
-c = client.Client("user1")
-c.show_keys()
+import client
+import websockets
+import asyncio
+
+async def register():
+    uri = "ws://localhost:8001"
+    async with websockets.connect(uri) as websocket:
+        
+        c = client.Client("user1")
+        c.show_keys()
+        name = c.username
+        await websocket.send(name)
+        print(f"> {name}")
+
+        greeting = await websocket.recv()
+        print(f"< {greeting}")
+
+asyncio.get_event_loop().run_until_complete(register())
+
